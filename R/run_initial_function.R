d410cc21d8cd278018543afcbcf8e495ba99d880
mcmullarkey <mullarkey.mike@gmail.com>
2025-07-05 14:21:08 -0600

    Closes #2: add pre-commit hooks to repo

---
+++ R/run_initial_function.R
@@ -0,0 +1,14 @@
+#' Initial function
+#'
+#' @param n A number
+#'
+#' @return Sum of n + n
+#' @export
+#'
+#' @examples
+#' run_initial_function(5)
+#' run_initial_function(10)
+#' run_initial_function(-3)
+run_initial_function <- function(n) {
+  return(n + n)
+}
